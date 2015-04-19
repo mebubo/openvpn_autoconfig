b48c5fd8f75a4e7f3f3f26b106f6bc7a3e6c5f46
Sergei Dolgov <dolgovs@gmail.com>
2015-04-19 16:47:03 +0200

    Split into functions, add udp

--- bin/openvpn.sh
+++ bin/openvpn.sh
@@ -10,48 +10,56 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-apt-get update -q
-debconf-set-selections <<EOF
+install_packages() {
+    apt-get update -q
+    debconf-set-selections <<EOF
 iptables-persistent iptables-persistent/autosave_v4 boolean true
 iptables-persistent iptables-persistent/autosave_v6 boolean true
 EOF
-apt-get install -qy openvpn curl iptables-persistent
+    apt-get install -qy openvpn curl iptables-persistent
+}
 
-cd /etc/openvpn
+configure_nat() {
+    >>/etc/sysctl.conf echo net.ipv4.ip_forward=1
+    sysctl -p
 
-# Certificate Authority
->ca-key.pem      openssl genrsa 2048
->ca-csr.pem      openssl req -new -key ca-key.pem -subj /CN=OpenVPN-CA/
->ca-cert.pem     openssl x509 -req -in ca-csr.pem -signkey ca-key.pem -days 365
->ca-cert.srl     echo 01
+    iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+    iptables -t nat -A POSTROUTING -s 10.8.1.0/24 -o eth0 -j MASQUERADE
+    >/etc/iptables/rules.v4 iptables-save
+}
 
-# Server Key & Certificate
->server-key.pem  openssl genrsa 2048
->server-csr.pem  openssl req -new -key server-key.pem -subj /CN=OpenVPN-Server/
->server-cert.pem openssl x509 -req -in server-csr.pem -CA ca-cert.pem -CAkey ca-key.pem -days 365
 
-# Client Key & Certificate
->client-key.pem  openssl genrsa 2048
->client-csr.pem  openssl req -new -key client-key.pem -subj /CN=OpenVPN-Client/
->client-cert.pem openssl x509 -req -in client-csr.pem -CA ca-cert.pem -CAkey ca-key.pem -days 365
+generate_certs() {
+    cd /etc/openvpn
 
-# Diffie hellman parameters
->dh.pem     openssl dhparam 2048
+    # Certificate Authority
+    >ca-key.pem      openssl genrsa 2048
+    >ca-csr.pem      openssl req -new -key ca-key.pem -subj /CN=OpenVPN-CA/
+    >ca-cert.pem     openssl x509 -req -in ca-csr.pem -signkey ca-key.pem -days 365
+    >ca-cert.srl     echo 01
 
-chmod 600 *-key.pem
+    # Server Key & Certificate
+    >server-key.pem  openssl genrsa 2048
+    >server-csr.pem  openssl req -new -key server-key.pem -subj /CN=OpenVPN-Server/
+    >server-cert.pem openssl x509 -req -in server-csr.pem -CA ca-cert.pem -CAkey ca-key.pem -days 365
 
-# Set up IP forwarding and NAT for iptables
->>/etc/sysctl.conf echo net.ipv4.ip_forward=1
-sysctl -p
+    # Client Key & Certificate
+    >client-key.pem  openssl genrsa 2048
+    >client-csr.pem  openssl req -new -key client-key.pem -subj /CN=OpenVPN-Client/
+    >client-cert.pem openssl x509 -req -in client-csr.pem -CA ca-cert.pem -CAkey ca-key.pem -days 365
 
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
->/etc/iptables/rules.v4 iptables-save
+    # Diffie hellman parameters
+    >dh.pem     openssl dhparam 2048
 
-# Write configuration files for client and server
+    chmod 600 *-key.pem
+}
 
-SERVER_IP=$(curl -s4 https://canhazip.com || echo "<insert server IP here>")
+generate_configs() {
+    cd /etc/openvpn
 
->tcp443.conf cat <<EOF
+    SERVER_IP=$(curl -s4 https://canhazip.com || echo "<insert server IP here>")
+
+    >tcp443.conf cat <<EOF
 server      10.8.0.0 255.255.255.0
 verb        3
 duplicate-cn
@@ -85,13 +93,46 @@ dev         tun443
 status      openvpn-status-443.log
 EOF
 
->client.ovpn cat <<EOF
+    >udp1194.conf cat <<EOF
+server      10.8.1.0 255.255.255.0
+verb        3
+duplicate-cn
+key         server-key.pem
+ca          ca-cert.pem
+cert        server-cert.pem
+dh          dh.pem
+keepalive   10 120
+persist-key yes
+persist-tun yes
+comp-lzo    yes
+push        "dhcp-option DNS 8.8.8.8"
+push        "dhcp-option DNS 8.8.4.4"
+
+# Normally, the following command is sufficient.
+# However, it doesn't assign a gateway when using 
+# VMware guest-only networking.
+#
+# push        "redirect-gateway def1 bypass-dhcp"
+
+push        "redirect-gateway bypass-dhcp"
+push        "route-metric 512"
+push        "route 0.0.0.0 0.0.0.0"
+
+user        nobody
+group       nogroup
+
+proto       udp
+port        1194
+dev         tun1194
+status      openvpn-status-1194.log
+EOF
+
+    >client.ovpn cat <<EOF
 client
 nobind
 dev tun
-redirect-gateway def1 bypass-dhcp
-remote $SERVER_IP 443 tcp
 comp-lzo yes
+redirect-gateway def1 bypass-dhcp
 
 <key>
 $(cat client-key.pem)
@@ -102,8 +143,29 @@ $(cat client-cert.pem)
 <ca>
 $(cat ca-cert.pem)
 </ca>
+
+<connection>
+remote $SERVER_IP 1194 udp
+</connection>
+
+<connection>
+remote $SERVER_IP 443 tcp-client
+</connection>
+
 EOF
 
-service openvpn restart
-cat client.ovpn
-cd -
+    service openvpn restart
+}
+
+all() {
+    install_packages
+    configure_nat
+    generate_certs
+    generate_configs
+}
+
+if [[ -z "$1" ]]; then
+    all
+else
+    "$@"
+fi
